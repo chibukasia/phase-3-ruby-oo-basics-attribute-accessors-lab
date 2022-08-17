67186afdd5c34561806a9c6e6926a0fcc4786c2b
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-17 21:12:03 +0300

    commit tree 277d976018a0ebf905492ba49e8293af835ced55

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,34 @@
 ## code your solution here. 
+# class Person
+#     attr_reader :first_name, :last_name
+
+#     def name=(full_name)
+#         first_name, last_name = full_name.split
+#         @first_name = first_name
+#         @last_name = last_name
+#     end
+
+#     def name
+#         "#{@first_name} #{@last_name}".strip
+#     end
+#     # attr_writer :name
+#     # attr_accessor :name
+# end
+
+# helo = Person.new
+# helo.name = "meak lanmame"
+# #helo.last_name = ""
+# puts helo.name
+# #puts lanme.name
+
+class Cat
+    attr_accessor :name
+
+    def meow
+        #@cat_name
+        puts "meow!"
+    end
+end
+
+kil = Cat.new
+kil.meow
